3027dd98784ae5ede01a235b71961393b6bf17cb
Parth <parth2002g@gmail.com>
2022-01-19 18:30:49 +0530

    Troubleshooted mistakes

--- Flask/Projects/Flask_project1/main.py
+++ Flask/Projects/Flask_project1/main.py
@@ -1,15 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET','POST'])
 def handle_data():
     email = request.form.get('email')
     name = request.form.get('name')
     password = request.form.get('password')
-    print(email)
-    print(name)
-    print(password)
     return render_template('registration.html')
 
 if __name__ == '__main__':
